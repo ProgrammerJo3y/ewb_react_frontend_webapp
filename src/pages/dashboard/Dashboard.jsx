bb1121c565fc3d046c5228470394c3051a0e64a9
Nikki <13564190@student.uts.edu.au>
2022-04-20 11:33:12 +1000

    Setup layout

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,7 +3,29 @@ import './dashboard.css'
 export default function Dashboard() {
   return (
     <div className="dashboard">
-        
+        <h1>Dashboard</h1>
+        <p>Machine Issues</p>
+        <p>Income Errors</p>
+        <p>New Feedback</p>
+        <h2>Washing Machine Details</h2>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>Location</th>
+            <th>Bookings</th>
+            <th>Errors</th>
+            <th>Feedback</th>
+            <th>View</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Port Villa</td>
+            <td>101</td>
+            <td></td>
+            <td>Handle worn down</td>
+            <td></td>
+          </tr>
+        </table>
     </div>
   )
 }
