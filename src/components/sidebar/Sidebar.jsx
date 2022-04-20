7abe42a62f9f71aa770b1bbabb3b225af2836b64
JayWilling <jwilling123@gmail.com>
2022-04-20 13:05:32 +1000

    Hover and active link in sidebar colours set

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,16 +1,16 @@
 import './sidebar.css'
-import {Link} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
-          <Link className="sidebarTitle" to="/dashboard">Dashboard</Link>
-          <Link className="sidebarTitle" to="/bookings">Bookings</Link>
-          <Link className="sidebarTitle" to="/reports">Reports</Link>
-          <Link className="sidebarTitle" to="/feedback">Feedback</Link>
-          <Link className="sidebarTitle" to="/settings">Settings</Link>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/dashboard">Dashboard</NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/bookings">Bookings</NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/reports">Reports</NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/feedback">Feedback</NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/settings">Settings</NavLink>
         </div>
         <div className="footer">&copy; Mamma's Leaf</div>
       </div>
