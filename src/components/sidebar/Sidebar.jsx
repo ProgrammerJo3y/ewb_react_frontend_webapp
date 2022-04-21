787c19e507f8c8150964b993ea0042f62238eb8f
JayWilling <jwilling123@gmail.com>
2022-04-21 15:57:23 +1000

    Added colour palette to global styling sheet, added icons to sidebar, change sidebar to reflect EWB theme

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,9 @@
 import './sidebar.css'
+import DashboardIcon from '../../assets/icons/home.svg'
+import ReportsIcon from '../../assets/icons/reports.svg'
+import FeedbackIcon from '../../assets/icons/feedback.svg'
+import BookingsIcon from '../../assets/icons/bookings.svg'
+import SettingsIcon from '../../assets/icons/settings.svg'
 import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
@@ -6,11 +11,26 @@ export default function Sidebar() {
     <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
-          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/dashboard">Dashboard</NavLink>
-          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/bookings">Bookings</NavLink>
-          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/reports">Reports</NavLink>
-          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/feedback">Feedback</NavLink>
-          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/settings">Settings</NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")}to="/dashboard">
+            <img className="sidebarIcon" src={DashboardIcon} alt="Dashboard logo"/>
+            Dashboard
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/bookings">
+            <img className="sidebarIcon" src={BookingsIcon} alt="Bookings icon"/>
+            Bookings
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/reports">
+            <img className="sidebarIcon" src={ReportsIcon} alt="Reports Icon"/>
+            Reports
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/feedback">
+            <img className="sidebarIcon" src={FeedbackIcon} alt="Feedback icon"/>
+            Feedback
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "sidebarActive" : "sidebarInactive")} to="/settings">
+          <img className="sidebarIcon" src={SettingsIcon} alt="Settings icon"/>
+            Settings
+          </NavLink>
         </div>
         <div className="footer">&copy; Mamma's Leaf</div>
       </div>
