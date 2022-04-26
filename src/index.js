edc719062de9cc38f6e58ddeaf0c9247a3679202
David Osburn <david.osburn@student.uts.edu.au>
2022-04-26 22:12:50 +1000

    setup ApolloClient in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import {ApolloClient, HttpLink, InMemoryCache, ApolloProvider} from "@apollo/client"
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://n-api-sigma.vercel.app/"
+  }),
+  cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </BrowserRouter>
 );
