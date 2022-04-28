4cbaaa64e6cfef59277edfc91ce89fa18e1844c7
pricilla-tran <71370147+pricilla-tran@users.noreply.github.com>
2022-04-28 16:08:52 +1000

    Added Labels and Basic Heading
    I'll jump on and add the customisable fields after some meetings I have and some research for a plan of attack

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,9 +1,26 @@
+import React, { useState } from 'react';
 import './settings.css'
 
 export default function Settings() {
   return (
-    <div className="settings">
+    <div className="settings-div">
+      <h1> Settings </h1>
+      <div className="fields-div">
+        <label>
+          <p className='settings-field'>Username</p>
 
+        </label>
+        <label>
+          <p className='settings-field'>Password</p>
+
+        </label>
+        <label>
+          <p className='settings-field'>Email</p>
+
+        </label>
+      </div>
     </div>
   )
 }
+
+
