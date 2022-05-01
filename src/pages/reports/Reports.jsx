41def665017904c9d5714272e632539c516ef3e6
JayWilling <jwilling123@gmail.com>
2022-05-01 22:18:04 +1000

    Added div structure to reports page

--- src/pages/reports/Reports.jsx
+++ src/pages/reports/Reports.jsx
@@ -1,9 +1,27 @@
 import './reports.css'
+import * as React from 'react'
 
 export default function Reports() {
-  return (
-    <div className="reports">
+  
+  const [report, setReport] = React.useState('report');
 
+  const options = [
+    {label: 'Blah', value: 'blah'},
+  ];
+
+  const handleReportChange = (event) => {
+    setReport(event.target.value);
+  }
+
+  return (
+    <div className="reportsContainer">
+      <div className="reportsHeader">
+        <h1>Reports</h1>
+      </div> 
+      <div className="reports">
+        <div className="reportsFilter"></div>
+        <div className="reportsGraph"></div>
+      </div>
     </div>
   )
 }
