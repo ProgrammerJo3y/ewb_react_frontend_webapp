e1794dabd9f100247b3a1b5abd4315d370436b32
JayWilling <jwilling123@gmail.com>
2022-05-02 21:31:36 +1000

    Added preset dropdown to reports page

--- src/pages/reports/Reports.jsx
+++ src/pages/reports/Reports.jsx
@@ -1,15 +1,16 @@
 import './reports.css'
-import * as React from 'react'
+import { useState} from 'react'
+import { Select, FormControl, MenuItem, InputLabel } from '@mui/material'
+import { CenterFocusStrong } from '@mui/icons-material';
 
 export default function Reports() {
   
-  const [report, setReport] = React.useState('report');
+  const [report, setReport] = useState('report');
 
-  const options = [
-    {label: 'Blah', value: 'blah'},
-  ];
+  const options = ["Preset 1", "Preset 2", "Preset 3"];
 
   const handleReportChange = (event) => {
+    // Handle chart population here
     setReport(event.target.value);
   }
 
@@ -17,10 +18,32 @@ export default function Reports() {
     <div className="reportsContainer">
       <div className="reportsHeader">
         <h1>Reports</h1>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Preset</InputLabel>
+          <Select
+            labelId="preset-select-label"
+            id="preset-select"
+            value={report}
+            label="Preset"
+            onChange={handleReportChange}
+          >
+            {options.map(item => {
+              return (
+                <MenuItem value={item}>{item}</MenuItem>
+              )
+            })}
+          </Select>
+        </FormControl>
       </div> 
       <div className="reports">
-        <div className="reportsFilter"></div>
-        <div className="reportsGraph"></div>
+        <div className="reportsFilter">
+          <h1>Filter</h1>
+          <body>Filter options go here</body>
+        </div>
+        <div className="reportsGraph">
+          <h1>Report</h1>
+          <body>Charts go here</body>
+        </div>
       </div>
     </div>
   )
