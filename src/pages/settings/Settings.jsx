0ad5cc152af52fdc25b45ec4295cd266188833a0
pricilla-tran <71370147+pricilla-tran@users.noreply.github.com>
2022-05-03 23:05:42 +1000

    Basic Formatting Completed
    Might muck around with the colours and all that but I will need to figure out how to get the editing on the textboxes but mostly looking good I might need to change the structure but again it is mostly there!

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -2,6 +2,10 @@ import React, { useState, Component} from 'react';
 import './settings.css'
 
 export default function Settings(){
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+  
   const username_state = {
     value: "Some name",
     isInEditMode: false
@@ -21,18 +25,21 @@ export default function Settings(){
     <div className="settings-div">
       <h1> Settings </h1>
       <div className="fields-div">
-        <label>
-          <p className='settings-field'>Username</p>
-
+        <label className='settings-fields'>
+          <p className='settings-text'>Username</p>
+          <input type="text" value="0456976789"/>
         </label>
-        <label>
-          <p className='settings-field'>Password</p>
-
+        <label className='settings-fields'>
+          <p className='settings-text'>Password</p>
+          <input type="text" value="********" />
         </label>
-        <label>
-          <p className='settings-field'>Email</p>
-
+        <label className='settings-fields'>
+          <p className='settings-text'>Email</p>
+          <input type="text" value="j_doe@ewb.org.au" />
         </label>
+      </div>
+      <div className='submit-button-div'>
+        <button type="submit" className='settings-submit'>Submit</button>
       </div>
     </div>
   )
