3f5f609461f1df1f5f75fd7359be0bde91ec266a
JayWilling <jwilling123@gmail.com>
2022-05-12 14:44:24 +1000

    Cleaned up chart imports

--- src/components/barchart/Barchart.jsx
+++ src/components/barchart/Barchart.jsx
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const sampleData = [
   {
