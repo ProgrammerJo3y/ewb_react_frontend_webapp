a288c970c81062deab3e79796140b06ae3f2165e
pricilla-tran <71370147+pricilla-tran@users.noreply.github.com>
2022-05-17 21:11:14 +1000

    Button Changes
    Need to get the setActive

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -7,15 +7,15 @@ export default function Users() {
   return (
     <div className="usersContainer">
       <div className='usersRolesContainer'>
-        <button>Clients</button>
-        <button>Operators</button>
+        <button className={({ isActive }) => (isActive ? "users_tabActive" : "users_tabinActive")}>Clients</button>
+        <button className={({ isActive }) => (isActive ? "users_tabActive" : "users_tabinActive")}>Operators</button>
       </div>
 
       <div className='usersFilterContainer'>
         <input placeholder='Filter Users'></input>
         <div className='userControls'>
-          <button>Edit</button>
-          <button>Delete</button>
+          <button className='users_button'>Edit</button>
+          <button className='users_button'>Delete</button>
         </div>
       </div>
       <div className='usersTableContainer'>
