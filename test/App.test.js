0a91f9e21061427b551c781ee9e1ad38f0e52910
David Osburn <david.osburn@student.uts.edu.au>
2022-05-21 19:38:47 +1000

    added missing dependencies, set up first test

--- test/App.test.js
+++ test/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from '../src/App';
 
 describe('App component', () => {
- test('it renders', () => {
+ test('App renders Login Screen', () => {
    render(<App/>);
+   expect(screen.getByText('EWB Administrator Portal Login')).toBeInTheDocument();
  });
 })
