722dddefd790df6b095792d591322a6e3a04c253
David Osburn <david.osburn@student.uts.edu.au>
2022-05-22 19:37:02 +1000

    a few more testcases

--- test/App.test.js
+++ test/App.test.js
@@ -4,8 +4,19 @@ import '@testing-library/jest-dom';
 import App from '../src/App';
 
 describe('App component', () => {
- test('App renders Login Screen', () => {
-   render(<App/>);
-   expect(screen.getByText('EWB Administrator Portal Login')).toBeInTheDocument();
- });
+  test('App renders Login Screen', () => {
+    render(<App/>);
+    expect(screen.getByText('EWB Administrator Portal Login')).toBeInTheDocument();
+  });
+  
+  test('Content hidden until logged in', () => {
+    render(<App/>);
+    expect(screen.queryByText('Dashboard')).not.toBeInTheDocument();
+    expect(screen.queryByText('Bookings')).not.toBeInTheDocument();
+    expect(screen.queryByText('Settings')).not.toBeInTheDocument();
+    expect(screen.queryByText('Users')).not.toBeInTheDocument();
+    expect(screen.queryByText('Reports')).not.toBeInTheDocument();
+
+    // Add login here, then check if content displays
+  });
 })
