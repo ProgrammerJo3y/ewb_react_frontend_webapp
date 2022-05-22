c231940bee65890d05b31bfc5f6a174c5d7c5dfd
JayWilling <jwilling123@gmail.com>
2022-05-22 11:53:55 +1000

    Settings screen finalised

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Component} from 'react';
+import React, { useState } from 'react';
 import './settings.css'
 
 export default function Settings(){
@@ -6,6 +6,7 @@ export default function Settings(){
   const [Password, setPassword] = useState("********");
   const [Email, setEmail] = useState("j_doe@ewb.org.au");
 
+  // handleChange may not be necessary given changes in data only need to be recorded when submit button is clicked
   const handleChange = (event) => {
     
     setPhoneNumber(event.target.PhoneNumber)
@@ -18,46 +19,44 @@ export default function Settings(){
     
   }
 
+  const handleNewAdmin = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <div className="settings-div">
-      <h1 className='admin-heading'> Settings </h1>
+      <h1 className='admin-heading'> Account Settings </h1>
       <form className="fields-div" onSubmit={handleSubmit}>
-        <label className='settings-fields'>
-          <p className='settings-text'>Phone Number</p>
-          <input type="text" value={PhoneNumber} placeholder='0456976789' onChange={handleChange}/>
-        </label>
-        <label className='settings-fields'>
-          <p className='settings-text'>Password</p>
-          <input type="password" value={Password} placeholder='password'/>
-        </label>
-        <label className='settings-fields'>
-          <p className='settings-text'>Email</p>
-          <input type="text" value={Email} placeholder='j_doe@ewb.org.au'/>
-        </label>
+
+        <label className='settings-fields'>Phone Number</label>
+        <input id="phoneNumberId" type="text" value={PhoneNumber} placeholder="Phone Number" onChange={handleChange}/>
+        
+        <label className='settings-fields'>Password</label>
+        <input id="passwordId" type="password" value={Password} placeholder='Password' onChange={handleChange}/>
+
+        <label className='settings-fields'>Email</label>
+        <input id="emailId" type="text" value={Email} placeholder='Email' onChange={handleChange}/>
+        
         <div className='settings-submit-button-div'>
-        <button type="submit" className='settings-submit'>Submit</button>
+          <button type="submit" className='settings-submit'>Submit</button>
         </div>
       </form>
       
       
       <h1 className='admin-heading'> Add a New Admin </h1>
-      <form className='add-admin-fields-div' onSubmit={handleSubmit}>
-        <label className='settings-fields'>
-          <p className='settings-text'>Full Name</p>
-          <input type="text"/>
-        </label>
-        <label className='settings-fields'>
-          <p className='settings-text'>Phone Number</p>
-          <input type="text"/>
-        </label>
-        <label className='settings-fields'>
-          <p className='settings-text'>Password</p>
-          <input type="password"/>
-        </label>
-        <label className='settings-fields'>
-          <p className='settings-text'>Email</p>
-          <input type="text"/>
-        </label>
+      <form className='add-admin-fields-div' onSubmit={handleNewAdmin}>
+        <label className='settings-fields'>Full Name</label>
+        <input id="newFullNameId"type="text"/>
+        
+        <label className='settings-fields'>Phone Number</label>
+        <input id="newPhoneNumberId" type="text"/>
+
+        <label className='settings-fields'>Password</label>
+        <input id="newPasswordId" type="password"/>
+
+        <label className='settings-fields'>Email</label>
+        <input id="newEmailId" type="text"/>
+
         <div className='settings-submit-button-div'>
           <button type="submit" className='settings-submit'>Submit</button>
         </div>
